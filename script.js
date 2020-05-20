37b638cbc93f1545137dac5a884b4c0d30ea2f83
Irungaray <LionelIrungaray@GMail.com>
2020-05-20 20:06:57 -0300

    Hechas las promesas

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById('video')
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
 function startVideo() {
     navigator.getUserMedia(
         { video: {} },
@@ -8,4 +15,6 @@ function startVideo() {
     )
 }
 
-startVideo()
+video.addEventListener('play', () => {
+    console.log('Capo de la gran puta')
+})

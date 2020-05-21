7fa0d2cdf663dba93e358c189c8083dcc42147d2
Irungaray <lionelirungaray@gmail.com>
2020-05-21 12:14:42 -0300

    Terminado

--- script.js
+++ script.js
@@ -16,5 +16,16 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-    console.log('Capo de la gran puta')
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
 })
